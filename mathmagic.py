85107cc6df29ce7d0ed2912c05ec097c4247d4e9
Abram Hindle <abram.hindle@ualberta.ca>
2015-11-13 11:52:57 -0700

    License! Apache 2.0

--- mathmagic.py
+++ mathmagic.py
@@ -1,9 +1,28 @@
+#!/usr/bin/python
+#     Math Magic!
+#     Copyright 2015 Abram Hindle
+#     
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#     
+#         http://www.apache.org/licenses/LICENSE-2.0
+#     
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License
+# .
 import scipy
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib
 import argparse
+
+
+
 
 WINDOWNAME="Function"
 
@@ -114,6 +133,27 @@ if __name__ == "__main__":
     python mathmagic.py -H "x + 0.5*(olds*x + t%100/100.0)" -S "oldv*y + t%66/66.0" -V "oldh*(t%10)"
 
     Inspired by/Stolen from Ashley Mills https://www.youtube.com/watch?v=OIlJVH99yIk
+
+    (C) 2015 Abram Hindle 
+
+    License: Apache 2.0
+
+
+
+    Copyright 2015 Abram Hindle
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
 
     '''
     parser = argparse.ArgumentParser(description=description,
